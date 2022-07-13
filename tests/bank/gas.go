8a087d3c0d6b40dfa4b780467806d6008b017fb1
Wojtek <103407812+wojtek-coreum@users.noreply.github.com>
2022-07-13 11:20:32 +0200

    Integration test checking that gas consumed by token transfer is below assumed level (#13)
    * Integration test checking that gas consumed by token transfer is below assumed level
    
    * fixes
    
    * fixes
    
    * Merge remote-tracking branch 'origin/master' into test-used-gas
    
    * Make linter happy
    
    * Pass memo as a parameter
    
    * fix
    
    * Merge remote-tracking branch 'origin/master' into test-used-gas

---
+++ tests/bank/gas.go
@@ -0,0 +1,80 @@
+package bank
+
+import (
+	"context"
+	"fmt"
+	"math/big"
+	"strings"
+	"time"
+
+	"github.com/CoreumFoundation/coreum-tools/pkg/logger"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+
+	"github.com/CoreumFoundation/crust/infra/apps/cored"
+	"github.com/CoreumFoundation/crust/infra/testing"
+)
+
+var maxMemo = strings.Repeat("-", 256) // cosmos sdk is configured to accept maximum memo of 256 characters by default
+
+// TestTransferMaximumGas checks that transfer does not take more gas than assumed
+func TestTransferMaximumGas(chain cored.Cored, numOfTransactions int) (testing.PrepareFunc, testing.RunFunc) {
+	const margin = 1.5
+	const maxGasAssumed = 100000 // set it to 50%+ higher than maximum observed value
+
+	amount, ok := big.NewInt(0).SetString("100000000000000000000", 10)
+	if !ok {
+		panic("invalid amount")
+	}
+
+	var wallet1, wallet2 cored.Wallet
+
+	return func(ctx context.Context) error {
+			wallet1 = chain.AddWallet(fmt.Sprintf("%score", amount))
+			wallet2 = chain.AddWallet("1core")
+			return nil
+		},
+		func(ctx context.Context, t *testing.T) {
+			testing.WaitUntilHealthy(ctx, t, 20*time.Second, chain)
+
+			client := chain.Client()
+
+			var err error
+			wallet1.AccountNumber, wallet1.AccountSequence, err = client.GetNumberSequence(ctx, wallet1.Key.Address())
+			require.NoError(t, err)
+			wallet2.AccountNumber, wallet2.AccountSequence, err = client.GetNumberSequence(ctx, wallet2.Key.Address())
+			require.NoError(t, err)
+
+			var maxGasUsed int64
+			toSend := cored.Balance{Denom: "core", Amount: amount}
+			for i, sender, receiver := numOfTransactions, wallet1, wallet2; i >= 0; i, sender, receiver = i-1, receiver, sender {
+				gasUsed, err := sendAndReturnGasUsed(ctx, client, sender, receiver, toSend)
+				require.NoError(t, err)
+
+				if gasUsed > maxGasUsed {
+					maxGasUsed = gasUsed
+				}
+				sender.AccountSequence++
+			}
+			assert.LessOrEqual(t, margin*float64(maxGasUsed), float64(maxGasAssumed))
+			logger.Get(ctx).Info("Maximum gas used", zap.Int64("maxGasUsed", maxGasUsed))
+		}
+}
+
+func sendAndReturnGasUsed(ctx context.Context, client cored.Client, sender, receiver cored.Wallet, toSend cored.Balance) (int64, error) {
+	txBytes, err := client.PrepareTxBankSend(ctx, cored.TxBankSendData{
+		Sender:   sender,
+		Receiver: receiver,
+		Balance:  toSend,
+		Memo:     maxMemo, // memo is set to max length here to charge as much gas as possible
+	})
+	if err != nil {
+		return 0, err
+	}
+	result, err := client.Broadcast(ctx, txBytes)
+	if err != nil {
+		return 0, err
+	}
+	return result.GasUsed, nil
+}
--- tests/index.go
+++ tests/index.go
@@ -15,10 +15,26 @@ import (
 func Tests(appF *apps.Factory) (infra.Mode, []*testing.T) {
 	mode := appF.CoredNetwork("coretest", 3, 0)
 	node := mode[0].(cored.Cored)
-	return mode,
-		[]*testing.T{
-			testing.New(auth.TestUnexpectedSequenceNumber(node)),
-			testing.New(bank.TestInitialBalance(node)),
-			testing.New(bank.TestCoreTransfer(node)),
-		}
+	nodes := []cored.Cored{
+		node,
+		mode[1].(cored.Cored),
+		mode[2].(cored.Cored),
+	}
+
+	tests := []*testing.T{
+		testing.New(auth.TestUnexpectedSequenceNumber(node)),
+		testing.New(bank.TestInitialBalance(node)),
+		testing.New(bank.TestCoreTransfer(node)),
+	}
+
+	// The idea is to run 200 transfer transactions to be sure that none of them uses more gas than we assumed.
+	// To make each faster the same test is started 10 times, each broadcasting 20 transactions, to make use of parallelism
+	// implemented inside testing framework. Test itself is written serially to not fight for resources with other tests.
+	// In the future, once we have more tests running in parallel, we will replace 10 tests running 20 transactions each
+	// with a single one running 200 of them.
+	for i := 0; i < 10; i++ {
+		tests = append(tests, testing.New(bank.TestTransferMaximumGas(nodes[i%len(nodes)], 20)))
+	}
+
+	return mode, tests
 }
