7d0fe27f8f3328e62cb7a996d141e6cec6cd317f
Wojtek <103407812+wojtek-coreum@users.noreply.github.com>
2022-08-18 11:15:09 +0200

    Remove hacky nolints related to gofmt of go 1.19 (#64)
    * Remove hacky nolints related to gofmt of go 1.19

--- infra/apps/blockexplorer/bdjuno.go
+++ infra/apps/blockexplorer/bdjuno.go
@@ -5,6 +5,6 @@ import (
 )
 
 // BDJunoConfigTemplate contains bdjuno configuration template
-//nolint:gofmt,goimports // Looks like gofmt linter has a bug and it produces error because of go:embed
+//
 //go:embed bdjuno/config/config.tmpl.yaml
 var BDJunoConfigTemplate string
