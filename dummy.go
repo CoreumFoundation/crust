1d3e8232f761b10f6edefa98495247dc01cf0963
Wojtek <103407812+wojtek-coreum@users.noreply.github.com>
2022-08-24 13:47:19 +0200

    Temporary hacks until fee model is merged into `coreum` (#50)
    * Temporary hacks until fee model is merged into `coreum`
    
    * Merge master into fee-model
    
    * Merge master into fee-model
    
    * Merge master into fee-model
    
    * Merge master into fee-model
    
    * Merge master into fee-model
    
    * Merge master into fee-model
    
    * Merge master into fee-model
    
    * Merge remote-tracking branch 'origin/master' into fee-model
    
    # Conflicts:
    #	pkg/zstress/stress.go
    
    * Merge master into fee-model
    
    * Remove stale zstress

---
+++ dummy.go
@@ -0,0 +1,6 @@
+package crust
+
+import (
+	// FIXME (wojtek): Remove this once module fee is merged in coreum. Without this CI complains that this module should be removed from go.mod
+	_ "github.com/ignite-hq/cli/integration"
+)
