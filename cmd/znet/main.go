7e2cb1d4152dac32738f110ab8c3a218acf4ea75
Wojtek <103407812+wojtek-coreum@users.noreply.github.com>
2022-09-27 15:13:29 +0200

    Stop using IoC in `znet` (#99)
    * Stop using IoC in `znet`
    
    * Merge master into wojtek/remove-ioc
    
    * Merge remote-tracking branch 'origin/master' into wojtek/remove-ioc
    
    # Conflicts:
    #	pkg/znet/commands.go
    #	pkg/znet/setup.go
    
    * after-merge fixes
    
    * Merge remote-tracking branch 'origin/wojtek/remove-ioc' into wojtek/remove-ioc
    
    * Merge master into wojtek/remove-ioc

--- cmd/znet/main.go
+++ cmd/znet/main.go
@@ -1,86 +1,156 @@
 package main
 
 import (
+	"context"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/spf13/cobra"
 
-	"github.com/CoreumFoundation/coreum-tools/pkg/ioc"
 	"github.com/CoreumFoundation/coreum-tools/pkg/logger"
 	"github.com/CoreumFoundation/coreum-tools/pkg/must"
 	"github.com/CoreumFoundation/coreum-tools/pkg/run"
+	"github.com/CoreumFoundation/coreum/integration-tests/testing"
+	"github.com/CoreumFoundation/coreum/pkg/config"
 	"github.com/CoreumFoundation/crust/infra"
+	"github.com/CoreumFoundation/crust/infra/apps"
 	"github.com/CoreumFoundation/crust/pkg/znet"
 )
 
 func main() {
-	run.Tool("znet", znet.IoC, func(c *ioc.Container, configF *infra.ConfigFactory, cmdF *znet.CmdFactory) error {
-		rootCmd := &cobra.Command{
-			SilenceUsage:  true,
-			SilenceErrors: true,
-			Short:         "Creates preconfigured session for environment",
-			RunE:          cmdF.Cmd(znet.Activate),
-		}
-		logger.AddFlags(logger.ToolDefaultConfig, rootCmd.PersistentFlags())
-		rootCmd.PersistentFlags().StringVar(&configF.EnvName, "env", defaultString("CRUST_ZNET_ENV", "znet"), "Name of the environment to run in")
-		rootCmd.PersistentFlags().StringVar(&configF.HomeDir, "home", defaultString("CRUST_ZNET_HOME", must.String(os.UserCacheDir())+"/crust/znet"), "Directory where all files created automatically by znet are stored")
-		addBinDirFlag(rootCmd, configF)
-		addModeFlag(rootCmd, c, configF)
-		addFilterFlag(rootCmd, configF)
+	run.Tool("znet", nil, func(ctx context.Context) error {
+		configF := infra.NewConfigFactory()
+		cmdF := znet.NewCmdFactory(configF)
+		config.NewNetwork(testing.NetworkConfig).SetupPrefixes()
 
-		startCmd := &cobra.Command{
-			Use:   "start",
-			Short: "Starts environment",
-			RunE:  cmdF.Cmd(znet.Start),
-		}
-		addBinDirFlag(startCmd, configF)
-		addModeFlag(startCmd, c, configF)
-		rootCmd.AddCommand(startCmd)
-
-		rootCmd.AddCommand(&cobra.Command{
-			Use:   "stop",
-			Short: "Stops environment",
-			RunE:  cmdF.Cmd(znet.Stop),
-		})
-
-		rootCmd.AddCommand(&cobra.Command{
-			Use:   "remove",
-			Short: "Removes environment",
-			RunE:  cmdF.Cmd(znet.Remove),
-		})
-
-		testCmd := &cobra.Command{
-			Use:   "test",
-			Short: "Runs integration tests for all repos",
-			RunE:  cmdF.Cmd(znet.Test),
-		}
-		addTestRepoFlag(testCmd, configF)
-		addBinDirFlag(testCmd, configF)
-		addFilterFlag(testCmd, configF)
-		rootCmd.AddCommand(testCmd)
-
-		rootCmd.AddCommand(&cobra.Command{
-			Use:   "spec",
-			Short: "Prints specification of running environment",
-			RunE:  cmdF.Cmd(znet.Spec),
-		})
-
-		rootCmd.AddCommand(&cobra.Command{
-			Use:   "console",
-			Short: "Starts tmux console on top of running environment",
-			RunE:  cmdF.Cmd(znet.Console),
-		})
-
-		rootCmd.AddCommand(&cobra.Command{
-			Use:   "ping-pong",
-			Short: "Sends tokens back and forth to generate transactions",
-			RunE:  cmdF.Cmd(znet.PingPong),
-		})
+		rootCmd := rootCmd(ctx, configF, cmdF)
+		rootCmd.AddCommand(startCmd(ctx, configF, cmdF))
+		rootCmd.AddCommand(stopCmd(ctx, configF, cmdF))
+		rootCmd.AddCommand(removeCmd(ctx, configF, cmdF))
+		rootCmd.AddCommand(testCmd(ctx, configF, cmdF))
+		rootCmd.AddCommand(specCmd(configF, cmdF))
+		rootCmd.AddCommand(consoleCmd(ctx, configF, cmdF))
+		rootCmd.AddCommand(pingPongCmd(ctx, configF, cmdF))
 
 		return rootCmd.Execute()
 	})
+}
+
+func rootCmd(ctx context.Context, configF *infra.ConfigFactory, cmdF *znet.CmdFactory) *cobra.Command {
+	rootCmd := &cobra.Command{
+		SilenceUsage:  true,
+		SilenceErrors: true,
+		Short:         "Creates preconfigured session for environment",
+		RunE: cmdF.Cmd(func() error {
+			spec := infra.NewSpec(configF)
+			config := znet.NewConfig(configF, spec)
+			return znet.Activate(ctx, configF, config)
+		}),
+	}
+	logger.AddFlags(logger.ToolDefaultConfig, rootCmd.PersistentFlags())
+	rootCmd.PersistentFlags().StringVar(&configF.EnvName, "env", defaultString("CRUST_ZNET_ENV", "znet"), "Name of the environment to run in")
+	rootCmd.PersistentFlags().StringVar(&configF.HomeDir, "home", defaultString("CRUST_ZNET_HOME", must.String(os.UserCacheDir())+"/crust/znet"), "Directory where all files created automatically by znet are stored")
+	addBinDirFlag(rootCmd, configF)
+	addModeFlag(rootCmd, configF)
+	addFilterFlag(rootCmd, configF)
+	return rootCmd
+}
+
+func startCmd(ctx context.Context, configF *infra.ConfigFactory, cmdF *znet.CmdFactory) *cobra.Command {
+	startCmd := &cobra.Command{
+		Use:   "start",
+		Short: "Starts environment",
+		RunE: cmdF.Cmd(func() error {
+			spec := infra.NewSpec(configF)
+			config := znet.NewConfig(configF, spec)
+			return znet.Start(ctx, config, spec)
+		}),
+	}
+	addBinDirFlag(startCmd, configF)
+	addModeFlag(startCmd, configF)
+	return startCmd
+}
+
+func stopCmd(ctx context.Context, configF *infra.ConfigFactory, cmdF *znet.CmdFactory) *cobra.Command {
+	return &cobra.Command{
+		Use:   "stop",
+		Short: "Stops environment",
+		RunE: cmdF.Cmd(func() error {
+			spec := infra.NewSpec(configF)
+			config := znet.NewConfig(configF, spec)
+			return znet.Stop(ctx, config, spec)
+		}),
+	}
+}
+
+func removeCmd(ctx context.Context, configF *infra.ConfigFactory, cmdF *znet.CmdFactory) *cobra.Command {
+	return &cobra.Command{
+		Use:   "remove",
+		Short: "Removes environment",
+		RunE: cmdF.Cmd(func() error {
+			spec := infra.NewSpec(configF)
+			config := znet.NewConfig(configF, spec)
+			return znet.Remove(ctx, config, spec)
+		}),
+	}
+}
+
+func testCmd(ctx context.Context, configF *infra.ConfigFactory, cmdF *znet.CmdFactory) *cobra.Command {
+	testCmd := &cobra.Command{
+		Use:   "test",
+		Short: "Runs integration tests for all repos",
+		RunE: cmdF.Cmd(func() error {
+			configF.ModeName = "test"
+			spec := infra.NewSpec(configF)
+			config := znet.NewConfig(configF, spec)
+			return znet.Test(ctx, config, spec)
+		}),
+	}
+	addTestRepoFlag(testCmd, configF)
+	addBinDirFlag(testCmd, configF)
+	addFilterFlag(testCmd, configF)
+	return testCmd
+}
+
+func specCmd(configF *infra.ConfigFactory, cmdF *znet.CmdFactory) *cobra.Command {
+	return &cobra.Command{
+		Use:   "spec",
+		Short: "Prints specification of running environment",
+		RunE: cmdF.Cmd(func() error {
+			spec := infra.NewSpec(configF)
+			return znet.Spec(spec)
+		}),
+	}
+}
+
+func consoleCmd(ctx context.Context, configF *infra.ConfigFactory, cmdF *znet.CmdFactory) *cobra.Command {
+	return &cobra.Command{
+		Use:   "console",
+		Short: "Starts tmux console on top of running environment",
+		RunE: cmdF.Cmd(func() error {
+			spec := infra.NewSpec(configF)
+			config := znet.NewConfig(configF, spec)
+			return znet.Console(ctx, config, spec)
+		}),
+	}
+}
+
+func pingPongCmd(ctx context.Context, configF *infra.ConfigFactory, cmdF *znet.CmdFactory) *cobra.Command {
+	return &cobra.Command{
+		Use:   "ping-pong",
+		Short: "Sends tokens back and forth to generate transactions",
+		RunE: cmdF.Cmd(func() error {
+			spec := infra.NewSpec(configF)
+			config := znet.NewConfig(configF, spec)
+			appF := apps.NewFactory(config, spec, testing.NetworkConfig)
+			mode, err := znet.Mode(appF, config.ModeName)
+			if err != nil {
+				return err
+			}
+			return znet.PingPong(ctx, mode)
+		}),
+	}
 }
 
 func addTestRepoFlag(cmd *cobra.Command, configF *infra.ConfigFactory) {
@@ -98,8 +168,8 @@ func addBinDirFlag(cmd *cobra.Command, configF *infra.ConfigFactory) {
 		"Path to directory where executables exist")
 }
 
-func addModeFlag(cmd *cobra.Command, c *ioc.Container, configF *infra.ConfigFactory) {
-	cmd.Flags().StringVar(&configF.ModeName, "mode", defaultString("CRUST_ZNET_MODE", "dev"), "List of applications to deploy: "+strings.Join(c.Names((*infra.Mode)(nil)), " | "))
+func addModeFlag(cmd *cobra.Command, configF *infra.ConfigFactory) {
+	cmd.Flags().StringVar(&configF.ModeName, "mode", defaultString("CRUST_ZNET_MODE", "dev"), "List of applications to deploy: "+strings.Join(znet.Modes(), " | "))
 }
 
 func addFilterFlag(cmd *cobra.Command, configF *infra.ConfigFactory) {
