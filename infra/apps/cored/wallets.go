b8d13aae6b98ef43f45034e0d90bf9704d37eb3f
Artem S <vertex451@gmail.com>
2023-04-20 09:29:38 +0300

    c

--- infra/apps/cored/wallets.go
+++ infra/apps/cored/wallets.go
@@ -2,6 +2,7 @@ package cored
 
 // mnemonics generating well-known keys to create predictable wallets so manual operation is easier.
 const (
+	// just add comment to trigger CI
 	AliceMnemonic   = "mandate canyon major bargain bamboo soft fetch aisle extra confirm monster jazz atom ball summer solar tell glimpse square uniform situate body ginger protect"
 	BobMnemonic     = "move equip digital assault wrong speed border multiply knife steel trash donor isolate remember lucky moon cupboard achieve canyon smooth pulp chief hold symptom"
 	CharlieMnemonic = "announce already cherry rotate pull apology banana dignity region horse aspect august country exit connect unit agent curious violin tide town link unable whip"
