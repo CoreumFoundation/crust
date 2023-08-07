9890e840bd778a55c78ddbf805698da0f0f0499f
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2023-08-07 17:28:52 +0200

    Add DAEMON_SHUTDOWN_GRACE

--- infra/apps/cored/cored.go
+++ infra/apps/cored/cored.go
@@ -254,6 +254,10 @@ func (c Cored) Deployment() infra.Deployment {
 					Name:  "DAEMON_NAME",
 					Value: "cored",
 				},
+				{
+					Name:  "DAEMON_SHUTDOWN_GRACE",
+					Value: "1m",
+				},
 			}
 		},
 		Volumes: []infra.Volume{
