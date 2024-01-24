91bf5c1622913b72bf369377837c16b3f454c7df
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2024-01-24 13:25:57 +0100

    add debug info

--- infra/apps/cored/coverage.go
+++ infra/apps/cored/coverage.go
@@ -16,6 +16,7 @@ const CovdataDirName = "covdatafiles"
 func CoverageDump(ctx context.Context, coredHomeDir string, dstFilePath string) error {
 	srcCovdata := filepath.Join(coredHomeDir, CovdataDirName)
 
+	fmt.Printf("src-covdata: %v, dst-covdata: %v\n", srcCovdata, dstFilePath)
 	cmd := exec.Go("tool", "covdata", "textfmt", fmt.Sprintf("-i=%s", srcCovdata), fmt.Sprintf("-o=%s", dstFilePath))
 	return errors.WithStack(libexec.Exec(ctx, cmd))
 }
