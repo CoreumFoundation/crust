e7c37a83da3372c64cabf417b4a8058fb690a917
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2024-01-25 20:32:06 +0100

    fix permission issue

--- infra/apps/cored/cored.go
+++ infra/apps/cored/cored.go
@@ -345,7 +345,7 @@ func (c Cored) Deployment() infra.Deployment {
 				},
 				{
 					Name:  "GOCOVERDIR",
-					Value: filepath.Join(targets.AppHomeDir, CovdataDirName),
+					Value: filepath.Join(targets.AppHomeDir, string(c.config.NetworkConfig.ChainID()), CovdataDirName),
 				},
 			}
 		},
@@ -368,7 +368,7 @@ func (c Cored) Deployment() infra.Deployment {
 			},
 			{
 				Source:      filepath.Join(c.config.HomeDir, CovdataDirName),
-				Destination: filepath.Join(targets.AppHomeDir, CovdataDirName),
+				Destination: filepath.Join(targets.AppHomeDir, string(c.config.NetworkConfig.ChainID()), CovdataDirName),
 			},
 		},
 		ArgsFunc: func() []string {
