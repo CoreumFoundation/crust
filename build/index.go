c1ffac00e67af9dbdfd7f40a8d491c9b88607e6b
Wojtek <103407812+wojtek-coreum@users.noreply.github.com>
2024-05-27 13:51:10 +0200

    Export main function of `znet` (#403)

--- build/index.go
+++ build/index.go
@@ -13,9 +13,9 @@ import (
 
 // Commands is a definition of commands available in build system.
 var Commands = map[string]types.Command{
-	"build":    {Fn: crust.BuildZNet, Description: "Builds znet binary"},
-	"build/me": {Fn: crust.BuildBuilder, Description: "Builds the builder"},
-	"download": {Fn: crust.DownloadDependencies, Description: "Downloads go dependencies"},
+	"build/me":   {Fn: crust.BuildBuilder, Description: "Builds the builder"},
+	"build/znet": {Fn: crust.BuildZNet, Description: "Builds znet binary"},
+	"download":   {Fn: crust.DownloadDependencies, Description: "Downloads go dependencies"},
 	"images": {Fn: func(ctx context.Context, deps types.DepsFunc) error {
 		deps(
 			gaia.BuildDockerImage,
