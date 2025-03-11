bf29ec5d10437bcee1af39cea2fd5f9dad6a9960
Masih Yeganeh <masih@coreum.com>
2025-03-11 15:40:26 +0330

    Fix znet path (#462)

--- build/crust/build.go
+++ build/crust/build.go
@@ -24,6 +24,16 @@ func BuildBuilder(ctx context.Context, deps types.DepsFunc) error {
 func BuildZNet(ctx context.Context, deps types.DepsFunc) error {
 	return golang.Build(ctx, deps, golang.BinaryBuildConfig{
 		TargetPlatform: tools.TargetPlatformLocal,
+		PackagePath:    "build/cmd/znet",
+		BinOutputPath:  "bin/.cache/znet",
+		CGOEnabled:     true,
+	})
+}
+
+// BuildCrustZNet builds znet.
+func BuildCrustZNet(ctx context.Context, deps types.DepsFunc) error {
+	return golang.Build(ctx, deps, golang.BinaryBuildConfig{
+		TargetPlatform: tools.TargetPlatformLocal,
 		PackagePath:    "znet/cmd/znet",
 		BinOutputPath:  "bin/.cache/znet",
 		CGOEnabled:     true,
