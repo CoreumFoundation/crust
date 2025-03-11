d2a3c7ea14b7b4852f3e84d0379e8a9f297c5d36
Masih Yeganeh <goodboy.php@gmail.com>
2025-03-11 13:53:42 +0330

    Fix znet path

--- build/index.go
+++ build/index.go
@@ -9,7 +9,7 @@ import (
 // Commands is a definition of commands available in build system.
 var Commands = map[string]types.Command{
 	"build/me":   {Fn: crust.BuildBuilder, Description: "Builds the builder"},
-	"build/znet": {Fn: crust.BuildZNet, Description: "Builds znet binary"},
+	"build/znet": {Fn: crust.BuildCrustZNet, Description: "Builds znet binary"},
 	"lint":       {Fn: golang.Lint, Description: "Lints code"},
 	"test":       {Fn: golang.Test, Description: "Runs unit tests"},
 	"tidy":       {Fn: golang.Tidy, Description: "Runs go mod tidy"},
