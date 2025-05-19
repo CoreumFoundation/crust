152b10021791c08f466b92f91bfce3202f0cb50b
TxCorpi0x <6095314+TxCorpi0x@users.noreply.github.com>
2025-05-19 12:14:38 +0300

    exclude contract address const from length check

--- znet/infra/apps/blockexplorer/default.go
+++ znet/infra/apps/blockexplorer/default.go
@@ -21,4 +21,5 @@ var DefaultPorts = Ports{
 // https://github.com/CosmWasm/wasmd/blob/04cb6e5408cc54c27247b0b327dfa99769d5103c/x/wasm/keeper/addresses.go#L18
 // the address of instantiated contract is calculated by CodeID and the sequence number of instance, so the generated address
 // is always as follows (if the logic of relayer and leader does not change).
+// nolint:lll
 const DefaultXrplContractAddress = "devcore14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9sd4f0ak"
